b1ae1f9013d1191e14f6eed11f542b5182d30922
switchpiggy <switchpiggy@users.noreply.github.com>
2019-11-09 14:32:09 -0800

    new changes

--- backend/forum.js
+++ backend/forum.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var app  = require('express');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -8,6 +9,17 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if(err) throw err;
-  console.log('All Good!');
+  if (err) throw err;
+  console.log("Connected!");
+  let sql = `CREATE TABLE users (first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  student_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  gender ENUM('male', 'female'),
+  rating MEDIUMINT(200) NOT NULL,
+  grade TINYINT(8) NOT NULL,
+  is_admin ENUM('true', 'false') NOT NULL)`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("The customers table is created!!");
+  });
 });
